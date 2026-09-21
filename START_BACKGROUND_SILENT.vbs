e7403bb8875501b2d769e6c3c00bde1aac3ed2bb
Madrasa Admin <admin@jamiaislamia.com>
2026-09-21 12:27:13 +0600

    feat: add 24/7 background launchers, dockerfile, and render config

---
+++ START_BACKGROUND_SILENT.vbs
@@ -0,0 +1,3 @@
+Set WshShell = CreateObject("WScript.Shell")
+WshShell.Run chr(34) & "d:\madrasa managment\START_MADRASA_24_7.bat" & Chr(34), 0
+Set WshShell = Nothing
---
+++ START_MADRASA_24_7.bat
@@ -0,0 +1,9 @@
+@echo off
+title Madrasa Management 24/7 Production Server
+cd /d "d:\madrasa managment"
+echo ========================================================
+echo   Madrasa Management System - 24/7 Server Running
+echo   Access URL: http://localhost:3000
+echo ========================================================
+npm start
+pause
